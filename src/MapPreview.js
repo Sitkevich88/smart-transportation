cd6ceaaf1bb31a76a55437aaae2931e4ac20df03
Sitkevich88 <Sitkevich2002@mail.ru>
2022-11-12 21:40:39 +0300

    saved map info to files

--- src/MapPreview.js
+++ src/MapPreview.js
@@ -1,219 +1,13 @@
 import styles from "./MapPreview.module.css";
 import React from "react";
+import mapStations from './map/stations.json';
+import mapLines from './map/trainLines.json';
+import mapIntersections from './map/intersections.json';
 
 const MapPreview = () => {
-    const stations = [
-        {
-            id: 1,
-            x: 457,
-            y: 336,
-            name: "Каменная",
-            line_id: 1
-        },
-        {
-            id: 2,
-            x: 600,
-            y: 400,
-            name: "Песочная",
-            line_id: 1
-        },
-        {
-            id: 3,
-            x: 675,
-            y: 465,
-            name: "Мраморная",
-            line_id: 1
-        },
-        {
-            id: 4,
-            x: 764,
-            y: 545,
-            name: "Смородиновая",
-            line_id: 1
-        },
-        {
-            id: 5,
-            x: 820,
-            y: 660,
-            name: "Глиняная",
-            line_id: 1
-        },
-        {
-            id: 6,
-            x: 940,
-            y: 740,
-            name: "Деревянная",
-            line_id: 1
-        },
-        {
-            id: 7,
-            x: 955,
-            y: 340,
-            name: "Отчаянная",
-            line_id: 2
-        },
-        {
-            id: 8,
-            x: 850,
-            y: 330,
-            name: "Грустная",
-            line_id: 2
-        },
-        {
-            id: 9,
-            x: 685,
-            y: 350,
-            name: "Нейтральная",
-            line_id: 2
-        },
-        {
-            id: 10,
-            x: 600,
-            y: 400,
-            name: "Песочная",
-            line_id: 2
-        },
-        {
-            id: 11,
-            x: 540,
-            y: 545,
-            name: "Ежевичная",
-            line_id: 2
-        },
-        {
-            id: 12,
-            x: 510,
-            y: 630,
-            name: "Пальмовая",
-            line_id: 2
-        },
-        {
-            id: 13,
-            x: 445,
-            y: 780,
-            name: "Довольная",
-            line_id: 2
-        },
-        {
-            id: 14,
-            x: 380,
-            y: 580,
-            name: "Сосновая",
-            line_id: 3
-        },
-        {
-            id: 15,
-            x: 510,
-            y: 630,
-            name: "Пальмовая",
-            line_id: 3
-        },
-        {
-            id: 16,
-            x: 640,
-            y: 690,
-            name: "Каштановая",
-            line_id: 3
-        },
-        {
-            id: 17,
-            x: 775,
-            y: 780,
-            name: "Березовая",
-            line_id: 3
-        },
-        {
-            id: 18,
-            x: 390,
-            y: 435,
-            name: "Малиновая",
-            line_id: 4
-        },
-        {
-            id: 19,
-            x: 540,
-            y: 545,
-            name: "Ежевичная",
-            line_id: 4
-        },
-        {
-            id: 20,
-            x: 764,
-            y: 545,
-            name: "Смородиновая",
-            line_id: 4
-        },
-        {
-            id: 21,
-            x: 890,
-            y: 540,
-            name: "Клубничная",
-            line_id: 4
-        },
-        {
-            id: 22,
-            x: 1030,
-            y: 517,
-            name: "Рябиновая",
-            line_id: 4
-        }
-    ];
-    const trainLines = [
-        {
-            id: 1,
-            name: "purple",
-            color: "#9f1ee0"
-        },
-        {
-            id: 2,
-            name: "blue",
-            color: "rgba(28,18,219,0.9)"
-        },
-        {
-            id: 3,
-            name: "red",
-            color: "#da1b2b"
-        },
-        {
-            id: 4,
-            name: "yellow",
-            color: "#f3f310"
-        }
-    ];
-    const intersections = [
-        {
-            id: 1,
-            station_id: 2
-        },
-        {
-            id: 1,
-            station_id: 10
-        },
-        {
-            id: 2,
-            station_id: 12
-        },
-        {
-            id: 2,
-            station_id: 15
-        },
-        {
-            id: 3,
-            station_id: 4
-        },
-        {
-            id: 3,
-            station_id: 20
-        },
-        {
-            id: 4,
-            station_id: 11
-        },
-        {
-            id: 4,
-            station_id: 19
-        }
-    ];
+    const stations = mapStations;
+    const trainLines = mapLines;
+    const intersections = mapIntersections;
     const canvas = React.createRef();
 
     normaliseStations(stations, 630, 440); //todo get width and height of canvas
@@ -249,22 +43,19 @@ function normaliseStations(stations, width, height){
 
 function getPointsFromStations(stations, trainLines, intersections){
     const points = [];
-    const r = 15;
+    const r = 10;
     const strokeWidth = 2;
     const offset = strokeWidth / 2;
 
     stations.forEach(st => {
-        if (!intersections.find(intersection => st.id === intersection.station_id)) {
-            points.push(
-                <circle cx={st.x} cy={st.y} r={r} style={{
-                    fill: trainLines.find(line => st.line_id === line.id).name
-                }} key={st.id} stroke="black" strokeWidth={strokeWidth}/>
-            );
-        } else{
-            points.push(
-                <circle cx={st.x} cy={st.y} r={r} key={st.id} fillOpacity="0" stroke="black" strokeWidth={strokeWidth}/>
-            );
-        }
+        if (intersections.find(intersection => st.id === intersection.station_id))
+            return;
+
+        points.push(
+            <circle cx={st.x} cy={st.y} r={r} style={{
+                fill: trainLines.find(line => st.line_id === line.id).name
+            }} key={st.id} stroke="black" strokeWidth={strokeWidth}/>
+        );
     });
 
     function drawIntersection(intersectionId){
@@ -280,8 +71,9 @@ function getPointsFromStations(stations, trainLines, intersections){
         const strR = `M ${x} ${y-(r-offset)} L ${x} ${y+(r-offset)} A 1 1 0 0 0 ${x} ${y-(r-offset)} Z`;
 
         points.push(...[
-            (<path d={strL} key={intersectedStations[0].id} style={{fill: trainLines.find(line => intersectedStations[0].line_id === line.id).name}}/>),
-            (<path d={strR} key={intersectedStations[1].id} style={{fill: trainLines.find(line => intersectedStations[1].line_id === line.id).name}}/>)
+            (<path d={strL} key={intersectedStations[0].id } style={{fill: trainLines.find(line => intersectedStations[0].line_id === line.id).name}}/>),
+            (<path d={strR} key={intersectedStations[1].id} style={{fill: trainLines.find(line => intersectedStations[1].line_id === line.id).name}}/>),
+            <circle cx={x} cy={y} r={r} key={intersectedStations[0].name} fillOpacity="0" stroke="black" strokeWidth={strokeWidth}/>
         ]);
     }
 
